2b6f4fbe47865527c1f93006b24b0a4f2da4df9b
vzagde <vzagde110@gmail.com>
2016-02-12 22:56:24 +0530

    push whoosh plugin
    push whoosh plugin

--- www/js/index.js
+++ www/js/index.js
@@ -45,48 +45,5 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
-        alert('Received Event: ' + id);
-
-        var pushNotification = navigator.plugins.pushNotification;
-        alert('Received Event: ' + id);
-        pushNotification.register(app.successHandler, app.errorHandler,{"senderID":"223767762284","ecb":"app.onNotificationGCM"});
-    },
-
-    // result contains any message sent from the plugin call
-    successHandler: function(result) {
-        alert('Callback Success! Result = '+result)
-    },
-
-    errorHandler: function(error) {
-        alert(error);
-    },
-
-    onNotificationGCM: function(e) {
-        alert("Enterd onNotificationGCM : "+e);
-        switch( e.event )
-        {
-            case 'registered':
-                alert('registration Success'+e.regid.length);
-                if ( e.regid.length > 0 )
-                {
-                    console.log("Regid " + e.regid);
-                    alert('registration id = '+e.regid);
-                }
-            break;
- 
-            case 'message':
-              // this is the actual push notification. its format depends on the data model from the push server
-              alert('message = '+e.message+' msgcnt = '+e.msgcnt);
-            break;
- 
-            case 'error':
-              alert('GCM error = '+e.msg);
-            break;
- 
-            default:
-              alert('An unknown GCM event has occurred');
-              break;
-        }
     }
-
 };
